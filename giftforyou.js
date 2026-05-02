71c503323aa4e1a806cc4708fa6e6a97d1337711
ShinnAh <125694961+GinnnnSoIT@users.noreply.github.com>
2026-05-02 09:52:24 +0700

    Add files via upload

---
+++ giftforyou.js
@@ -0,0 +1,11 @@
+function showLove() {
+    document.getElementById("text").innerHTML =
+        "Nhấn vào xem là phải đọc hết nha <br> ở đây 3 tháng <br> nhiều chiện vui";
+    const btn = document.querySelector(".btnClick");
+    btn.style.display = "none";
+    const btnPic = document.querySelector(".btnPicture");
+    btnPic.style.display = "block";
+}
+
+
+
